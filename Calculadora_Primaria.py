0b06b9f8beaa85276592fa4c21784e3b0eb6f5fe
luxyfeer <luxyfeer@gmail.com>
2023-05-27 23:53:53 -0500

    Fx Ecuación lineal

--- Calculadora_Primaria.py
+++ Calculadora_Primaria.py
@@ -27,6 +27,17 @@ def coseno(a):
 def tangente(a):
     return math.tan(a)
 
+# Función ecuación lineal
+def resolver_ecuacion_lineal(a, b):
+    if a == 0:
+        if b == 0:
+            return "La ecuación es indeterminada (0 = 0)"
+        else:
+            return "La ecuación es contradictoria (0 = {} no es posible)".format(b)
+    else:
+        x = -b / a
+        return "El resultado de la ecuación {}x + {} = 0 es x = {}".format(a, b, x)
+
 # Función principal de la calculadora
 def calculadora():
     print("¡Bienvenido a la calculadora científica multifuncional!")
@@ -42,9 +53,10 @@ def calculadora():
         print("7. Seno")
         print("8. Coseno")
         print("9. Tangente")
-        print("10. Salir")
+        print("10. Resolver ecuación lineal de primer grado")
+        print("11. Salir")
 
-        opcion = input("Seleccione una opción (1-10): ")
+        opcion = input("Seleccione una opción (1-11): ")
 
         if opcion == "1":
             a = float(input("Ingrese el primer número: "))
@@ -78,6 +90,10 @@ def calculadora():
             a = float(input("Ingrese el ángulo en radianes: "))
             print("Resultado:", tangente(a))
         elif opcion == "10":
+            a = float(input("Ingrese el coeficiente a: "))
+            b = float(input("Ingrese el coeficiente b: "))
+            print(resolver_ecuacion_lineal(a, b))
+        elif opcion == "11":
             print("¡Hasta luego!")
             break
         else:
